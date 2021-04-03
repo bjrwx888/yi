4cc8b0b84469d07fc9ad626fb4d3c2303a7e830f
454313500@qq.com <454313500@qq.com>
2021-04-03 13:18:01 +0800

    v1.1.0

--- CC.Yi.DALFactory/DbSession.cs
+++ CC.Yi.DALFactory/DbSession.cs
@@ -10,7 +10,31 @@ namespace CC.Yi.DALFactory
     {
         public int SaveChanges()
         {
-            return DbContentFactory.GetCurrentDbContent().SaveChanges();
+            var context = DbContentFactory.GetCurrentDbContent();
+
+            var saved = false;
+            while (!saved)
+            {
+                try
+                {
+                    // Attempt to save changes to the database
+                    context.SaveChanges();
+                    saved = true;
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    foreach (var entry in ex.Entries)
+                    {
+                        var databaseValues = entry.GetDatabaseValues();
+
+                        // Refresh original values to bypass next concurrency check
+                        entry.OriginalValues.SetValues(databaseValues);
+
+                    }
+                }
+            }
+
+            return 1;
         }
         public DataContext GetDbContent()
         {
