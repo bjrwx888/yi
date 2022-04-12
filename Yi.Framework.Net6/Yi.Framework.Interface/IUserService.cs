796bba1abec60e665c650c0caceb689fb65ee09e
chenchun <chenchun@hymson.com>
2022-04-12 18:09:13 +0800

    更新仓储层上下文对象与接口命名规范

--- Yi.Framework.Net6/Yi.Framework.Interface/IUserService.cs
+++ Yi.Framework.Net6/Yi.Framework.Interface/IUserService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Yi.Framework.Model.Models;
 using Yi.Framework.Repository;
@@ -7,7 +8,27 @@ namespace Yi.Framework.Interface
 {
    public partial interface IUserService
     {
+        /// <summary>
+        /// 测试仓储的上下文对象
+        /// </summary>
+        /// <returns></returns>
+        public Task<List<UserEntity>> DbTest();
+
+        /// <summary>
+        /// 登录方法
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        /// <param name="userAction"></param>
+        /// <returns></returns>
         public Task<bool> Login(string userName, string password, Action<UserEntity> userAction = null);
+
+        /// <summary>
+        /// 注册方法
+        /// </summary>
+        /// <param name="userEntity"></param>
+        /// <param name="userAction"></param>
+        /// <returns></returns>
         public Task<bool> Register(UserEntity userEntity, Action<UserEntity> userAction = null);
     }
 }
