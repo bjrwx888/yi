512e640c13405a95498f2f78e52aa6706a436cbd
橙子 <454313500@qq.com>
2022-04-26 11:18:16 +0800

    添加菜单处理

--- Yi.Vue2.x/src/layouts/default/Drawer.vue
+++ Yi.Vue2.x/src/layouts/default/Drawer.vue
@@ -53,13 +53,176 @@ import userApi from "@/api/userApi";
 export default {
   methods: {
     init() {
-      userApi.GetMenuByHttpUser().then((resp) => {
-        this.items = resp.data.children;
-      });
+      //这里动态获取菜单，暂时写死
+      // userApi.GetMenuByHttpUser().then((resp) => {
+      //   this.items = resp.data.children;
+      // });
+      this.items = 
+      
+       [
+          {
+            icon: "mdi-view-dashboard",
+            router: "/",
+            menu_name: "首页",
+            roles: null,
+            mould: null,
+            is_top: 0,
+            sort: 0,
+            is_show: 1,
+            parentId: 1,
+            children: null,
+            id: 2,
+            is_delete: 0,
+          },
+          {
+            icon: "mdi-account-box-multiple",
+            router: null,
+            menu_name: "用户角色管理",
+            roles: null,
+            mould: null,
+            is_top: 0,
+            sort: 0,
+            is_show: 1,
+            parentId: 1,
+            children: [
+              {
+                icon: "mdi-account-box",
+                router: "/AdmUser/",
+                menu_name: "用户管理",
+                roles: null,
+                mould: null,
+                is_top: 0,
+                sort: 0,
+                is_show: 1,
+                parentId: 3,
+                children: null,
+                id: 4,
+                is_delete: 0,
+              },
+              {
+                icon: "mdi-account-circle",
+                router: "/admrole/",
+                menu_name: "角色管理",
+                roles: null,
+                mould: null,
+                is_top: 0,
+                sort: 0,
+                is_show: 1,
+                parentId: 3,
+                children: null,
+                id: 9,
+                is_delete: 0,
+              },
+            ],
+            id: 3,
+            is_delete: 0,
+          },
+          {
+            icon: "mdi-account-cash",
+            router: null,
+            menu_name: "角色接口管理",
+            roles: null,
+            mould: null,
+            is_top: 0,
+            sort: 0,
+            is_show: 1,
+            parentId: 1,
+            children: [
+              {
+                icon: "mdi-clipboard-check-multiple",
+                router: "/AdmMenu/",
+                menu_name: "菜单管理",
+                roles: null,
+                mould: null,
+                is_top: 0,
+                sort: 0,
+                is_show: 1,
+                parentId: 14,
+                children: null,
+                id: 15,
+                is_delete: 0,
+              },
+              {
+                icon: "mdi-circle-slice-8",
+                router: "/admMould/",
+                menu_name: "接口管理",
+                roles: null,
+                mould: null,
+                is_top: 0,
+                sort: 0,
+                is_show: 1,
+                parentId: 14,
+                children: null,
+                id: 20,
+                is_delete: 0,
+              },
+              {
+                icon: "mdi-clipboard-account",
+                router: "/admRoleMenu/",
+                menu_name: "角色菜单分配管理",
+                roles: null,
+                mould: null,
+                is_top: 0,
+                sort: 0,
+                is_show: 1,
+                parentId: 14,
+                children: null,
+                id: 25,
+                is_delete: 0,
+              },
+            ],
+            id: 14,
+            is_delete: 0,
+          },
+          {
+            icon: "mdi-clipboard-flow-outline",
+            router: null,
+            menu_name: "路由管理",
+            roles: null,
+            mould: null,
+            is_top: 0,
+            sort: 0,
+            is_show: 1,
+            parentId: 1,
+            children: [
+              {
+                icon: "mdi-account-eye",
+                router: "/userinfo/",
+                menu_name: "用户信息",
+                roles: null,
+                mould: null,
+                is_top: 0,
+                sort: 0,
+                is_show: 1,
+                parentId: 26,
+                children: null,
+                id: 27,
+                is_delete: 0,
+              },
+              {
+                icon: "mdi-account-eye",
+                router: "/pan",
+                menu_name: "云盘管理",
+                roles: null,
+                mould: null,
+                is_top: 0,
+                sort: 0,
+                is_show: 1,
+                parentId: 26,
+                children: null,
+                id: 28,
+                is_delete: 0,
+              },
+            ],
+            id: 26,
+            is_delete: 0,
+          },
+        ]
+
     },
     logout() {
       this.$store.dispatch("Logout");
-         this.$router.push({ path: "/login" });
+      this.$router.push({ path: "/login" });
     },
   },
   created() {
@@ -69,21 +232,18 @@ export default {
     image() {
       return this.$store.getters.image;
     },
-        gradient() {
+    gradient() {
       return this.$store.getters.gradient;
     },
-              drawerImage()
-    {
-     return  this.$store.state.home.drawerImage;
+    drawerImage() {
+      return this.$store.state.home.drawerImage;
     },
-                  dark()
-    {
-     return  this.$store.state.user.dark;
-    }
+    dark() {
+      return this.$store.state.user.dark;
+    },
   },
 
   data: () => ({
-
     items: [],
   }),
   name: "DefaultDrawer",
