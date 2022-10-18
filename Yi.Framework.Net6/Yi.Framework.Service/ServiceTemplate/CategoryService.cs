0a27cd740313acd01d4313edb5d207e208235c8d
陈淳 <chenchun@hymson.com>
2022-10-18 19:27:58 +0800

    商城模块

---
+++ Yi.Framework.Net6/Yi.Framework.Service/ServiceTemplate/CategoryService.cs
@@ -0,0 +1,14 @@
+﻿using SqlSugar;
+using Yi.Framework.Interface;
+using Yi.Framework.Model.Models;
+using Yi.Framework.Repository;
+
+namespace Yi.Framework.Service
+{
+    public partial class CategoryService : BaseService<CategoryEntity>, ICategoryService
+    {
+        public CategoryService(IRepository<CategoryEntity> repository) : base(repository)
+        {
+        }
+    }
+}
---
+++ Yi.Framework.Net6/Yi.Framework.Service/ServiceTemplate/SpecsService.cs
@@ -0,0 +1,14 @@
+﻿using SqlSugar;
+using Yi.Framework.Interface;
+using Yi.Framework.Model.Models;
+using Yi.Framework.Repository;
+
+namespace Yi.Framework.Service
+{
+    public partial class SpecsService : BaseService<SpecsEntity>, ISpecsService
+    {
+        public SpecsService(IRepository<SpecsEntity> repository) : base(repository)
+        {
+        }
+    }
+}
