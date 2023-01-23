aea8b55e824a851938fede259ebedddcdcdbcc62
橙子 <454313500@qq.com>
2023-01-23 19:33:10 +0800

    更新种子数据扩展，留个坑

---
+++ Yi.Framework.Net6/src/framework/Yi.Framework.Data/Extensions/DataSeedExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yi.Framework.Data.DataSeeds;
+
+namespace Yi.Framework.Data.Extensions
+{
+    public static class DataSeedExtensions
+    {
+        public static IApplicationBuilder UseDataSeedServer(this IApplicationBuilder builder)
+        {
+            var dataSeeds = builder.ApplicationServices.GetServices(typeof(IDataSeed<>));
+            //这个种子数据是有问题的，先放个坑
+
+            return builder.UseMiddleware<DataFilterMiddleware>();
+        }
+    }
+}
