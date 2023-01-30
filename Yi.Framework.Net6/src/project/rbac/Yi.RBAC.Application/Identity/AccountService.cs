aec0150a269f0b9aea05f63e43c50cbbd662abbc
橙子 <454313500@qq.com>
2023-01-30 19:08:50 +0800

    添加用户领域

---
+++ Yi.Framework.Net6/src/project/rbac/Yi.RBAC.Application/Identity/AccountService.cs
@@ -0,0 +1,15 @@
+﻿using NET.AutoWebApi.Setting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yi.Framework.Ddd.Services;
+
+namespace Yi.RBAC.Application.Identity
+{
+    [AppService]
+    public class AccountService: ApplicationService, IAutoApiService
+    {
+    }
+}
---
+++ Yi.Framework.Net6/src/project/rbac/Yi.RBAC.Domain/Identity/UserManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yi.RBAC.Domain.Identity
+{
+
+    /// <summary>
+    /// 用户领域服务
+    /// </summary>
+    [AppService]
+    public class UserManager
+    {
+    }
+}
