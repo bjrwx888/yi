d65c56512725d7ec5daeeebf1b0e75cc00e925e1
橙子 <454313500@qq.com>
2023-02-20 00:24:57 +0800

    refactor: 重构markdown

--- Yi.BBS.Vue2/src/views/markdown.vue
+++ Yi.BBS.Vue2/src/views/markdown.vue
@@ -25,29 +25,29 @@ export default {
       baseurl: "",
       configs: {},
       externalLink: {
-        markdown_css: function () {
+        markdown_css: function() {
           // 这是你的markdown css文件路径
-          return "/markdown/github-markdown.min.css";
+          return '/mavon-editor/markdown/github-markdown.min.css';
         },
-        hljs_js: function () {
+        hljs_js: function() {
           // 这是你的hljs文件路径
-          return "/highlightjs/highlight.min.js";
+          return '/mavon-editor/highlightjs/highlight.min.js';
         },
-        hljs_css: function (css) {
+        hljs_css: function(css) {
           // 这是你的代码高亮配色文件路径
-          return "/highlightjs/styles/" + css + ".min.css";
+          return '/mavon-editor/highlightjs/styles/' + css + '.min.css';
         },
-        hljs_lang: function (lang) {
+        hljs_lang: function(lang) {
           // 这是你的代码高亮语言解析路径
-          return "/highlightjs/languages/" + lang + ".min.js";
+          return '/highlightjs/languages/' + lang + '.min.js';
         },
-        katex_css: function () {
+        katex_css: function() {
           // 这是你的katex配色方案路径路径
-          return "/katex/katex.min.css";
+          return '/mavon-editor/katex/katex.min.css';
         },
-        katex_js: function () {
+        katex_js: function() {
           // 这是你的katex.js路径
-          return "/katex/katex.min.js";
+          return '/mavon-editor/katex/katex.min.js';
         },
       },
     };
--- Yi.BBS.Vue2/src/views/user/comment.vue
+++ Yi.BBS.Vue2/src/views/user/comment.vue
@@ -185,6 +185,7 @@
               :scrollStyle="true"
               :ishljs="true"
               style="z-index: 1"
+              :externalLink="externalLink"
             />
           </v-card-text>
           <v-card-actions>
@@ -463,6 +464,32 @@ export default {
     form: {
       content: "",
     },
+    externalLink: {
+        markdown_css: function() {
+          // 这是你的markdown css文件路径
+          return '/mavon-editor/markdown/github-markdown.min.css';
+        },
+        hljs_js: function() {
+          // 这是你的hljs文件路径
+          return '/mavon-editor/highlightjs/highlight.min.js';
+        },
+        hljs_css: function(css) {
+          // 这是你的代码高亮配色文件路径
+          return '/mavon-editor/highlightjs/styles/' + css + '.min.css';
+        },
+        hljs_lang: function(lang) {
+          // 这是你的代码高亮语言解析路径
+          return '/highlightjs/languages/' + lang + '.min.js';
+        },
+        katex_css: function() {
+          // 这是你的katex配色方案路径路径
+          return '/mavon-editor/katex/katex.min.css';
+        },
+        katex_js: function() {
+          // 这是你的katex.js路径
+          return '/mavon-editor/katex/katex.min.js';
+        },
+      },
   }),
   created() {
     this.initializa();
