d5ee57f04d3f252d1c80d94b1f4bba473e9f6d6a
橙子 <454313500@qq.com>
2023-04-05 22:13:30 +0800

    doc:添加readme

--- Yi.BBS.Vue2/src/api/agreesApi.js
+++
@@ -1,9 +0,0 @@
-import myaxios from '@/utils/myaxios'
-export default {
-    getAgrees(discussId) {
-        return myaxios({
-            url: `/Agree/getAgrees?discussId=${discussId}`,
-            method: 'get'
-        })
-    },
-}
--- Yi.BBS.Vue2/src/api/warehouseApi.js
+++
@@ -1,9 +0,0 @@
-import myaxios from '@/utils/myaxios'
-export default {
-    GetWarehousesByUserId() {
-        return myaxios({
-            url: '/Warehouse/GetWarehousesByUserId',
-            method: 'get'
-        })
-    }
-}
