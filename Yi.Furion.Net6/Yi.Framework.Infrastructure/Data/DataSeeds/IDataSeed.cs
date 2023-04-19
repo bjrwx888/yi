2fd861025a1245843455434471f1a3a7b554c4e9
陈淳 <chenchun@hymson.com>
2023-04-19 16:35:47 +0800

    feat:添加种子数据

---
+++ Yi.Furion.Net6/Yi.Framework.Infrastructure/Data/DataSeeds/IDataSeed.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yi.Framework.Infrastructure.Data.DataSeeds
+{
+    public interface IDataSeed
+    {
+        Task<bool> InvokerAsync();
+    }
+
+    public interface IDataSeed<TEntity> : IDataSeed
+    {
+    }
+}
