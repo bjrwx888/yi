f0d0c304d673e17975234acd0d50126d460b49a7
橙子 <454313500@qq.com>
2023-10-19 21:47:10 +0800

    fix: 删除多余的种子数据

--- Yi.Furion.Net6/Yi.Framework.Module/WebFirstManager/DataSeed/TemplateDataSeed.cs
+++ Yi.Furion.Net6/Yi.Framework.Module/WebFirstManager/DataSeed/TemplateDataSeed.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Furion.DependencyInjection;
+﻿using Furion.DependencyInjection;
 using Yi.Framework.Infrastructure.Data.DataSeeds;
 using Yi.Framework.Infrastructure.Ddd.Repositories;
-using Yi.Framework.Infrastructure.Helper;
 using Yi.Framework.Module.WebFirstManager.Entities;
 
 namespace Yi.Framework.Module.WebFirstManager.DataSeed
@@ -20,19 +14,6 @@ namespace Yi.Framework.Module.WebFirstManager.DataSeed
         public override List<TemplateEntity> GetSeedData()
         {
             var entities = new List<TemplateEntity>();
-            entities.Add(new TemplateEntity
-            {
-                Id = SnowflakeHelper.NextId,
-                TemplateStr = "你好世界 :@model",
-                BuildPath = ""
-            });
-
-            entities.Add(new TemplateEntity
-            {
-                Id = SnowflakeHelper.NextId,
-                TemplateStr = "你好世界2 :@Model",
-                BuildPath = ""
-            });
 
 
             return entities;
