ca9add7d9307334b9f974015b8b7d3b56ab72323
陈淳 <454313500@qq.com>
2023-12-14 14:15:56 +0800

    feat: 添加测试控制器

---
+++ Yi.Abp.Net8/src/Yi.Abp.Application/TestService.cs
@@ -0,0 +1,17 @@
+﻿using Volo.Abp.Application.Services;
+
+namespace Yi.Abp.Application
+{
+    public class TestService : ApplicationService
+    {
+        /// <summary>
+        /// 你好世界
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public string GetHelloWorld(string? name)
+        {
+            return name ?? "HelloWord";
+        }
+    }
+}
