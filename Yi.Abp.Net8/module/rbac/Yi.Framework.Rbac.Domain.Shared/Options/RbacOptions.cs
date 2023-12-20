edcba7a35a013debb6542b78f0d6a95b2503180a
橙子 <454313500@qq.com>
2023-12-20 21:43:16 +0800

    feat: 完善验证、注册开关

--- Yi.Abp.Net8/module/rbac/Yi.Framework.Rbac.Domain.Shared/Options/RbacOptions.cs
+++ Yi.Abp.Net8/module/rbac/Yi.Framework.Rbac.Domain.Shared/Options/RbacOptions.cs
@@ -17,5 +17,10 @@ namespace Yi.Framework.Rbac.Domain.Shared.Options
         /// 是否开启登录验证码
         /// </summary>
         public bool EnableCaptcha { get; set; } = false;
+
+        /// <summary>
+        /// 是否开启用户注册功能
+        /// </summary>
+        public bool EnableRegister { get; set; } = false;
     }
 }
