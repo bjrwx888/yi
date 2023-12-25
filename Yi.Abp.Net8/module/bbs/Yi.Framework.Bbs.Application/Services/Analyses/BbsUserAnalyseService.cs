547b8b0f91162da45df4b86ea6fbfff1ed8c9ec8
橙子 <454313500@qq.com>
2023-12-25 21:41:37 +0800

    fix: 修复头像字段

--- Yi.Abp.Net8/module/bbs/Yi.Framework.Bbs.Application/Services/Analyses/BbsUserAnalyseService.cs
+++ Yi.Abp.Net8/module/bbs/Yi.Framework.Bbs.Application/Services/Analyses/BbsUserAnalyseService.cs
@@ -1,59 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mapster;
+using Microsoft.AspNetCore.Mvc;
 using SqlSugar;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 using Yi.Framework.Bbs.Application.Contracts.Dtos.BbsUser;
-using Yi.Framework.Bbs.Application.Contracts.Dtos.Discuss;
-using Yi.Framework.Bbs.Domain.Entities;
 using Yi.Framework.Bbs.Domain.Managers;
-using Yi.Framework.Bbs.Domain.Shared.Enums;
-using Yi.Framework.Rbac.Domain.Entities;
 using Yi.Framework.Rbac.Domain.Shared.Consts;
-using Yi.Framework.SqlSugarCore.Abstractions;
 
 namespace Yi.Framework.Bbs.Application.Services.Analyses
 {
-    public class BbsForumAnalyseService : ApplicationService, IApplicationService
+    public class BbsUserAnalyseService : ApplicationService, IApplicationService
     {
-        private ForumManager _forumManager;
-        public BbsForumAnalyseService(ForumManager forumManager)
+        private BbsUserManager _bbsUserManager;
+        public BbsUserAnalyseService(BbsUserManager bbsUserManager)
         {
-            _forumManager = forumManager;
+            _bbsUserManager = bbsUserManager;
         }
 
         /// <summary>
-        /// 推荐主题，随机返回主题列表
+        /// 推荐好友，随机返回好友列表
         /// </summary>
         /// <returns></returns>
-        [HttpGet("analyse/bbs-discuss/random")]
-        public async Task<List<DiscussGetListOutputDto>> GetRandomDiscussAsync([FromQuery] PagedResultRequestDto input)
+        [HttpGet("analyse/bbs-user/random")]
+        public async Task<List<BbsUserGetListOutputDto>> GetRandomUserAsync([FromQuery] PagedResultRequestDto input)
         {
-            var output = await _forumManager._discussRepository._DbQueryable
-                .Where(discuss=>discuss.PermissionType== DiscussPermissionTypeEnum.Public)
-                     .LeftJoin<UserEntity>((discuss, user) => discuss.CreatorId == user.Id)
-                         .LeftJoin<BbsUserExtraInfoEntity>((discuss, user, info) => user.Id == info.UserId)
-
-                            .OrderBy(x => SqlFunc.GetRandom())
-                           .Select((discuss, user, info) => new DiscussGetListOutputDto
-                           {
-                               Id = discuss.Id,
-                               IsAgree = SqlFunc.Subqueryable<AgreeEntity>().WhereIF(CurrentUser.Id != null, x => x.CreatorId == CurrentUser.Id && x.DiscussId == discuss.Id).Any(),
-
-                               User = new BbsUserGetListOutputDto()
-                               {
-                                   Id = user.Id,
-                                   UserName = user.UserName,
-                                   Nick = user.Nick,
-                                   Icon = user.Icon,
-                                   Level = info.Level,
-                                   UserLimit = info.UserLimit
-                               }
-
-                           }, true)
-                .ToPageListAsync(input.SkipCount, input.MaxResultCount);
-            return output;
+            var randUserIds = await _bbsUserManager._userRepository._DbQueryable
+                .Where(x => x.UserName != UserConst.Admin)
+                .OrderBy(x => SqlFunc.GetRandom())
+                .Select(x => x.Id).
+                ToPageListAsync(input.SkipCount, input.MaxResultCount);
+            var output = await _bbsUserManager.GetBbsUserInfoAsync(randUserIds);
+            return output.Adapt<List<BbsUserGetListOutputDto>>();
         }
 
+        /// <summary>
+        /// 积分排行榜
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("analyse/bbs-user/integral-top")]
+        public async Task<List<BbsUserGetListOutputDto>> GetIntegralTopUserAsync([FromQuery] PagedResultRequestDto input)
+        {
+            var randUserIds = await _bbsUserManager._userRepository._DbQueryable
+                .Where(x => x.UserName != UserConst.Admin)
+                .OrderBy(x => SqlFunc.GetRandom())
+                .Select(x => x.Id).
+                ToPageListAsync(input.SkipCount, input.MaxResultCount);
+            var output = await _bbsUserManager.GetBbsUserInfoAsync(randUserIds);
+            return output.Adapt<List<BbsUserGetListOutputDto>>();
+        }
 
     }
 }
