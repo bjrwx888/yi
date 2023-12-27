4867a0df2b0db46303e506d030e62203b4e4cb86
Xwen <929716663@qq.com>
2023-12-27 22:41:57 +0800

    feat:新增加载动画

--- Yi.Bbs.Vue3/src/App.vue
+++ Yi.Bbs.Vue3/src/App.vue
@@ -1,28 +1,29 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-    <el-config-provider :locale="locale">
-  <RouterView />
-</el-config-provider>
+  <el-config-provider :locale="locale">
+    <RouterView />
+  </el-config-provider>
 </template>
 <script setup>
 import useConfigStore from "@/stores/config";
-import { ElConfigProvider } from 'element-plus'
-import {onMounted  } from "vue";
+import { ElConfigProvider } from "element-plus";
+import { onMounted } from "vue";
 
-import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
-const locale= zhCn;
+import zhCn from "element-plus/dist/locale/zh-cn.mjs";
+const locale = zhCn;
 
 const configStore = useConfigStore();
 
+// 判断是否有loading有的话去掉
+const loading = document.getElementById("Loading");
+if (loading !== null) {
+  document.body.removeChild(Loading);
+}
+
 //加载全局信息
-onMounted(async()=>{
-
-await configStore.getConfig();
-})
-
+onMounted(async () => {
+  await configStore.getConfig();
+});
 </script>
-<style scoped>
-
-</style>
+<style scoped></style>
