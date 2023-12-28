470c9084539575b85a9873fc1508a50ecb7860f5
陈淳 <454313500@qq.com>
2023-12-28 15:23:00 +0800

    feat: 完成审计日志模块搭建

--- Yi.Abp.Net8/module/audit-logging/Yi.AuditLogging.SqlSugarCore/YiAuditLoggingSqlSugarCoreModule.cs
+++ Yi.Abp.Net8/module/audit-logging/Yi.AuditLogging.SqlSugarCore/YiAuditLoggingSqlSugarCoreModule.cs
@@ -1,16 +1,16 @@
-﻿//using Volo.Abp.AuditLogging;
-//using Volo.Abp.Modularity;
-//using Yi.Framework.SqlSugarCore;
+﻿using Volo.Abp.AuditLogging;
+using Volo.Abp.Modularity;
+using Yi.Framework.SqlSugarCore;
 
-//namespace Yi.AuditLogging.SqlSugarCore
-//{
-//    [DependsOn(typeof(AbpAuditLoggingDomainModule))]
-//    [DependsOn(typeof(YiFrameworkSqlSugarCoreModule))]
-//    public class YiAuditLoggingSqlSugarCoreModule:AbpModule
-//    {
-//        public override void ConfigureServices(ServiceConfigurationContext context)
-//        {
-//            context.Services.AddYiDbContext<YiAuditLoggingDbContext>();
-//        }
-//    }
-//}
+namespace Yi.AuditLogging.SqlSugarCore
+{
+    [DependsOn(typeof(AbpAuditLoggingDomainModule))]
+    [DependsOn(typeof(YiFrameworkSqlSugarCoreModule))]
+    public class YiAuditLoggingSqlSugarCoreModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.AddYiDbContext<YiAuditLoggingDbContext>();
+        }
+    }
+}
