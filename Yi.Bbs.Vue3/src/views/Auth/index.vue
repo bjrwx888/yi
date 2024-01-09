bf6155b1bdb93fd8c9d9f182e196ce6c28f313b2
Xwen <929716663@qq.com>
2024-01-09 20:46:55 +0800

    feat:测试

--- Yi.Bbs.Vue3/src/views/Auth/index.vue
+++ Yi.Bbs.Vue3/src/views/Auth/index.vue
@@ -13,6 +13,7 @@ const code = ref(route.query.code);
 const type = ref(route.query.state);
 
 const message = ref("");
+const scheme = ref("");
 watch(
   () => code.value,
   async (val) => {
@@ -20,12 +21,21 @@ watch(
       // 使用正则表达式提取路由参数
       const regex = /\/auth\/([\w-]+)[?]?/;
       const result = regex.exec(route.fullPath);
-      const authParam = result != null ? result[1].toUpperCase() : null;
-      console.log(type.value, "类型");
+      const authParam = result != null ? result[1] : null;
+      switch (authParam) {
+        case "gitee":
+          scheme.value = "Gitee";
+          break;
+        case "qq":
+          scheme.value = "QQ";
+          break;
+      }
       if (type.value === "0") {
-        const res = await authOtherLogin({ code: val }, authParam);
+        const res = await authOtherLogin({ code: val }, scheme.value);
+        console.log(res, "登录的");
       } else if (type.value === "1") {
-        const res = await authOtherBind({ code: val }, authParam);
+        const res = await authOtherBind({ code: val }, scheme.value);
+        console.log(res, "绑定的");
       }
       message.value = "授权成功";
       // window.close();
