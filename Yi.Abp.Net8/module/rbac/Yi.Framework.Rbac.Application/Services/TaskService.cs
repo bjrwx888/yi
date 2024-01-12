e71936063d76678f6748689aa6166a895c7072c2
陈淳 <454313500@qq.com>
2024-01-12 17:18:39 +0800

    feat: 初步搭建任务调度管理实现

--- Yi.Abp.Net8/module/rbac/Yi.Framework.Rbac.Application/Services/TaskService.cs
+++ Yi.Abp.Net8/module/rbac/Yi.Framework.Rbac.Application/Services/TaskService.cs
@@ -1,171 +1,202 @@
-﻿using Volo.Abp.Application.Services;
+﻿using System.Reflection;
+using Mapster;
+using Microsoft.AspNetCore.Mvc;
+using Quartz;
+using Quartz.Impl.Matchers;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Yi.Framework.Rbac.Application.Contracts.Dtos.Task;
 using Yi.Framework.Rbac.Application.Contracts.IServices;
+using Yi.Framework.Rbac.Domain.Shared.Enums;
 
 namespace Yi.Framework.Rbac.Application.Services
 {
     public class TaskService : ApplicationService, ITaskService
     {
-        //private readonly ISchedulerFactory _schedulerFactory;
-        //public TaskService(ISchedulerFactory schedulerFactory)
-        //{
-        //    _schedulerFactory = schedulerFactory;
-        //}
-        ///// <summary>
-        ///// 单查job
-        ///// </summary>
-        ///// <param name="jobId"></param>
-        ///// <returns></returns>
-        //[HttpGet("{jobId}")]
-        //public TaskGetOutput GetById([FromRoute] string jobId)
-        //{
-        //    var result = _schedulerFactory.TryGetJob(jobId, out var scheduler);
-        //    var data = scheduler.GetModel();
-        //    var output = data.JobDetail.Adapt<TaskGetOutput>();
-        //    output.TriggerArgs = data.Triggers[0].Args;
-        //    output.NextRunTime = data.Triggers[0].NextRunTime;
-        //    output.LastRunTime = data.Triggers[0].LastRunTime;
-        //    output.NumberOfRuns = data.Triggers[0].NumberOfRuns;
-        //    return output;
-        //}
-
-        ///// <summary>
-        ///// 多查job
-        ///// </summary>
-        ///// <returns></returns>
-        //[HttpGet("")]
-        //public PagedResultDto<TaskGetListOutput> GetList([FromQuery] TaskGetListInput input)
-        //{
-        //    var data = _schedulerFactory.GetJobsOfModels().Skip((input.PageNum - 1) * input.PageSize).Take(input.PageSize).OrderByDescending(x => x.JobDetail.UpdatedTime)
-
-        //        .ToList();
-        //    var output = data.Select(x =>
-        //    {
-
-        //        var res = new TaskGetListOutput();
-        //        res = x.JobDetail.Adapt<TaskGetListOutput>();
-        //        res.TriggerArgs = x.Triggers[0].Args;
-        //        res.Status = x.Triggers[0].Status.ToString();
-        //        return res;
-        //    }).ToList();
-        //    return new PagedResultDto<TaskGetListOutput>(data.Count(), output);
-        //}
-
-        ///// <summary>
-        ///// 创建job
-        ///// </summary>
-        ///// <param name="input"></param>
-        ///// <returns></returns>
-        //public ScheduleResult Create(TaskCreateInput input)
-        //{
+        private readonly ISchedulerFactory _schedulerFactory;
+        public TaskService(ISchedulerFactory schedulerFactory)
+        {
+            _schedulerFactory = schedulerFactory;
+        }
 
 
-        //    //jobBuilder
-        //    var jobBuilder = JobBuilder.Create(input.AssemblyName, input.JobType).SetJobId(input.JobId).SetGroupName(input.GroupName)
-        //    .SetConcurrent(input.Concurrent).SetDescription(input.Description);
+        /// <summary>
+        /// 单查job
+        /// </summary>
+        /// <param name="jobId"></param>
+        /// <returns></returns>
+        [HttpGet("{jobId}")]
+        public async Task<TaskGetOutput> GetByIdAsync([FromRoute] string jobId)
+        {
+            var scheduler = await _schedulerFactory.GetScheduler();
 
-        //    //triggerBuilder
-        //    //毫秒
-        //    TriggerBuilder triggerBuilder = null;
-        //    switch (input.Type)
-        //    {
-        //        case Core.Rbac.Enums.JobTypeEnum.Cron:
-        //            triggerBuilder = Triggers.Cron(input.Cron, CronStringFormat.WithSeconds);
-        //            break;
-        //        case Core.Rbac.Enums.JobTypeEnum.Millisecond:
-        //            triggerBuilder = Triggers.Period(input.Millisecond);
-        //            break;
-        //    }
+            var jobDetail = await scheduler.GetJobDetail(new JobKey(jobId));
+            var trigger = (await scheduler.GetTriggersOfJob(new JobKey(jobId))).First();
+            //状态
+            var state = await scheduler.GetTriggerState(trigger.Key);
+            var output = new TaskGetOutput
+            {
+                JobId = jobDetail.Key.Name,
+                GroupName = jobDetail.Key.Group,
+                JobType = jobDetail.JobType.Name,
+                Properties = Newtonsoft.Json.JsonConvert.SerializeObject(jobDetail.JobDataMap),
+                Concurrent = !jobDetail.ConcurrentExecutionDisallowed,
+                Description = jobDetail.Description,
+                LastRunTime = trigger.GetPreviousFireTimeUtc()?.DateTime,
+            };
 
-        //    //作业计划,单个jobBuilder与多个triggerBuilder组合
-        //    var schedulerBuilder = SchedulerBuilder.Create(jobBuilder, triggerBuilder);
+            if (trigger is ISimpleTrigger simple)
+            {
+                output.TriggerArgs = simple.RepeatInterval.TotalMilliseconds.ToString() + "毫秒";
 
+            }
+            else if (trigger is ICronTrigger cron)
+            {
+                output.TriggerArgs = cron.CronExpressionString!;
+            }
+            return output;
+        }
 
-        //    //调度中心工厂，使用作业计划管理job,返回调度中心单个
-        //    var result = _schedulerFactory.TryAddJob(schedulerBuilder, out var scheduler);
+        /// <summary>
+        /// 多查job
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("")]
+        public async Task<PagedResultDto<TaskGetListOutput>> GetList([FromQuery] TaskGetListInput input)
+        {
+            var items = new List<TaskGetOutput>();
 
-        //    return result;
-        //}
+            var scheduler = await _schedulerFactory.GetScheduler();
 
-        ///// <summary>
-        ///// 移除job
-        ///// </summary>
-        ///// <param name="jobId"></param>
-        ///// <returns></returns>
-        //public ScheduleResult Remove(string jobId)
-        //{
-        //    var res = _schedulerFactory.TryRemoveJob(jobId, out var scheduler);
-        //    return res;
-        //}
+            var groups = await scheduler.GetJobGroupNames();
 
-        ///// <summary>
-        ///// 暂停job
-        ///// </summary>
-        ///// <param name="jobId"></param>
-        ///// <returns></returns>
-        //[HttpPut]
-        //public ScheduleResult Pause(string jobId)
-        //{
-        //    var res = _schedulerFactory.TryGetJob(jobId, out var scheduler);
-
-        //    scheduler.Pause();
-        //    return res;
-        //}
-
-        ///// <summary>
-        ///// 开始job
-        ///// </summary>
-        ///// <param name="jobId"></param>
-        ///// <returns></returns>
-        //[HttpPut]
-        //public ScheduleResult Start(string jobId)
-        //{
-        //    var res = _schedulerFactory.TryGetJob(jobId, out var scheduler);
-        //    scheduler.Start();
-        //    return res;
-        //}
-
-        ///// <summary>
-        ///// 更新job
-        ///// </summary>
-        ///// <param name="jobId"></param>
-        ///// <param name="input"></param>
-        ///// <returns></returns>
-        //public ScheduleResult Update(string jobId, TaskUpdateInput input)
-        //{
-        //    //jobBuilder
-        //    var jobBuilder = JobBuilder.Create(input.AssemblyName, input.JobType).SetJobId(jobId).SetGroupName(input.GroupName)
-        //        .SetConcurrent(input.Concurrent).SetDescription(input.Description);
-
-        //    //triggerBuilder
-        //    //毫秒
-        //    TriggerBuilder triggerBuilder = null;
-        //    switch (input.Type)
-        //    {
-        //        case Core.Rbac.Enums.JobTypeEnum.Cron:
-        //            triggerBuilder = Triggers.Cron(input.Cron, CronStringFormat.WithSeconds);
-        //            break;
-        //        case Core.Rbac.Enums.JobTypeEnum.Millisecond:
-        //            triggerBuilder = Triggers.Period(input.Millisecond);
-        //            break;
-        //    }
-
-        //    //作业计划,单个jobBuilder与多个triggerBuilder组合
-        //    var schedulerBuilder = SchedulerBuilder.Create(jobBuilder, triggerBuilder);
+            foreach (var groupName in groups)
+            {
+                foreach (var jobKey in await scheduler.GetJobKeys(GroupMatcher<JobKey>.GroupEquals(groupName)))
+                {
+                    string jobName = jobKey.Name;
+                    string jobGroup = jobKey.Group;
+                    var triggers = (await scheduler.GetTriggersOfJob(jobKey)).First();
+                    items.Add(await GetByIdAsync(jobName));
+                }
+            }
 
 
-        //    var result = _schedulerFactory.TryUpdateJob(schedulerBuilder, out var scheduler);
-        //    return result;
-        //}
+            var output = items.Skip((input.SkipCount - 1) * input.MaxResultCount).Take(input.MaxResultCount)
+                .OrderByDescending(x => x.LastRunTime)
+                .ToList();
+            return new PagedResultDto<TaskGetListOutput>(items.Count(), output.Adapt<List<TaskGetListOutput>>());
+        }
 
-        //[HttpPost]
-        //public bool RunOnce(string jobId)
-        //{
-        //    var result = _schedulerFactory.TryGetJob(jobId, out var scheduler);
+        /// <summary>
+        /// 创建job
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public async Task Create(TaskCreateInput input)
+        {
+            var scheduler = await _schedulerFactory.GetScheduler();
 
-        //    var triggerBuilder = Triggers.Period(100).SetRunOnStart(true).SetMaxNumberOfRuns(1);
-        //    scheduler.AddTrigger(triggerBuilder);
-        //    //设置启动时执行一次，然后最大只执行一次
-        //    return true;
-        //}
+            //设置启动时执行一次，然后最大只执行一次
+
+
+            //jobBuilder
+            var jobClassType = Assembly.LoadFrom(input.AssemblyName).GetType(input.JobType);
+
+            var jobBuilder = JobBuilder.Create(jobClassType).WithIdentity(new JobKey(input.JobId, input.GroupName))
+                .WithDescription(input.Description);
+            if (!input.Concurrent)
+            {
+                jobBuilder.DisallowConcurrentExecution();
+            }
+
+            //triggerBuilder
+            TriggerBuilder triggerBuilder = null;
+            switch (input.Type)
+            {
+                case JobTypeEnum.Cron:
+                    triggerBuilder = 
+                       TriggerBuilder.Create().StartNow()
+                       .WithCronSchedule(input.Cron);
+
+
+
+
+                    break;
+                case JobTypeEnum.Millisecond:
+                    triggerBuilder =
+                     TriggerBuilder.Create().StartNow()
+                                            .WithSimpleSchedule(x => x
+                                            .WithInterval(TimeSpan.FromMilliseconds(input.Millisecond))
+                                            .RepeatForever()
+                                            );
+                    break;
+            }
+
+            //作业计划,单个jobBuilder与多个triggerBuilder组合
+            await scheduler.ScheduleJob(jobBuilder.Build(), triggerBuilder.Build());
+        }
+
+        /// <summary>
+        /// 移除job
+        /// </summary>
+        /// <param name="jobId"></param>
+        /// <returns></returns>
+        public async Task Remove(string jobId)
+        {
+            var scheduler = await _schedulerFactory.GetScheduler();
+            await scheduler.ResumeJob(new JobKey(jobId));
+        }
+
+        /// <summary>
+        /// 暂停job
+        /// </summary>
+        /// <param name="jobId"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public async Task Pause(string jobId)
+        {
+            var scheduler = await _schedulerFactory.GetScheduler();
+            await scheduler.PauseJob(new JobKey(jobId));
+        }
+
+        /// <summary>
+        /// 开始job
+        /// </summary>
+        /// <param name="jobId"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public async Task Start(string jobId)
+        {
+            var scheduler = await _schedulerFactory.GetScheduler();
+            await scheduler.ResumeJob(new JobKey(jobId));
+        }
+
+        /// <summary>
+        /// 更新job
+        /// </summary>
+        /// <param name="jobId"></param>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public async Task Update(string jobId, TaskUpdateInput input)
+        {
+            await Remove(jobId);
+            await Create(input.Adapt<TaskCreateInput>());
+        }
+
+        [HttpPost]
+        public async Task RunOnce(string jobId)
+        {
+            var scheduler = await _schedulerFactory.GetScheduler();
+            var jobDetail = await scheduler.GetJobDetail(new JobKey(jobId));
+
+            //设置启动时执行一次，然后最大只执行一次
+            var trigger = TriggerBuilder.Create().WithIdentity(Guid.NewGuid().ToString()).StartNow()
+                .WithSimpleSchedule(x => x
+                .WithRepeatCount(1))
+              .Build();
+
+            await scheduler.ScheduleJob(jobDetail, trigger);
+        }
     }
 }
