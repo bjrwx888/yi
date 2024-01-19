7e895403a9c132eb78841991985b7211480eb3bc
陈淳 <454313500@qq.com>
2024-01-19 17:45:11 +0800

    feat: 完成测试框架搭建

--- Yi.Abp.Net8/module/rbac/Yi.Framework.Rbac.Application.Contracts/IServices/IAccountService.cs
+++ Yi.Abp.Net8/module/rbac/Yi.Framework.Rbac.Application.Contracts/IServices/IAccountService.cs
@@ -1,9 +1,11 @@
-﻿using Yi.Framework.Rbac.Domain.Shared.Dtos;
+﻿using Yi.Framework.Rbac.Application.Contracts.Dtos.Account;
+using Yi.Framework.Rbac.Domain.Shared.Dtos;
 
 namespace Yi.Framework.Rbac.Application.Contracts.IServices
 {
     public interface IAccountService
     {
         Task<UserRoleMenuDto> Get();
+        Task<object> PostLoginAsync(LoginInputVo input);
     }
 }
---
+++ Yi.Abp.Net8/test/Yi.Abp.Test/YiAbpTestBase.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Volo.Abp;
+using Yi.Framework.Rbac.Domain.Repositories;
+using Yi.Framework.Rbac.SqlSugarCore.Repositories;
+
+namespace Yi.Abp.Test
+{
+    public class YiAbpTestBase :AbpTestBaseWithServiceProvider
+    {
+        public ILogger Logger { get; private set; }
+        protected IServiceScope TestServiceScope { get; }
+        public YiAbpTestBase()
+        {
+            IAbpApplicationWithExternalServiceProvider application = null;
+            IHost host = Host.CreateDefaultBuilder()
+               .UseAutofac()
+               .ConfigureServices((host, service) =>
+               {
+                   service.AddLogging(builder => builder.ClearProviders().AddConsole().AddDebug());
+                   /*application= */
+                   service.AddApplicationAsync<YiAbpTestModule>().Wait();
+                   this.ConfigureServices(host, service);
+               })
+               .ConfigureAppConfiguration(this.ConfigureAppConfiguration)
+               .Build();
+
+            this.ServiceProvider = host.Services;
+            this.TestServiceScope = ServiceProvider.CreateScope();
+            this.Logger = (ILogger)this.ServiceProvider.GetRequiredService(typeof(ILogger<>).MakeGenericType(this.GetType()));
+
+            //host.InitializeAsync().Wait();
+            this.Configure();
+        }
+
+
+
+        protected virtual void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+        {
+            services.AddTransient<IUserRepository, UserRepository>();
+        }
+
+        protected virtual void ConfigureAppConfiguration(IConfigurationBuilder configurationBuilder)
+        {
+            configurationBuilder.AddJsonFile("appsettings.json");
+            //configurationBuilder.AddJsonFile("appsettings.Development.json");
+            
+        }
+
+        protected virtual void Configure()
+        {
+        }
+    }
+}
