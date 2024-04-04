b57d56f31755f4bc8dd1e41cfb5ccb2d17a72d42
橙子 <454313500@qq.com>
2024-04-04 14:00:32 +0800

    feat: 搭建chathub模块

---
+++ Yi.Abp.Net8/module/chat-hub/Yi.Framework.ChatHub.Domain.Shared/YiFrameworkChatHubDomainSharedModule.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.Domain;
+
+namespace Yi.Framework.ChatHub.Domain.Shared
+{
+    [DependsOn(
+        typeof(AbpDddDomainSharedModule))]
+    public class YiFrameworkChatHubDomainSharedModule : AbpModule
+    {
+
+    }
+}
---
+++ Yi.Abp.Net8/module/chat-hub/Yi.Framework.ChatHub.Domain/YiFrameworkChatHubDomainModule.cs
@@ -0,0 +1,19 @@
+﻿using Volo.Abp.Domain;
+using Yi.Framework.ChatHub.Domain.Shared;
+
+
+namespace Yi.Framework.ChatHub.Domain
+{
+    [DependsOn(
+        typeof(YiFrameworkChatHubDomainSharedModule),
+
+        typeof(AbpDddDomainModule)
+        )]
+    public class YiFrameworkChatHubDomainModule : AbpModule
+    {
+        public override async Task OnPostApplicationInitializationAsync(ApplicationInitializationContext context)
+        {
+
+        }
+    }
+}
