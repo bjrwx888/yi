0d6b62c8e75d99c146edc4d55f0208192e6bad9b
橙子 <454313500@qq.com>
2024-06-01 21:45:20 +0800

    feat: 新增yi-abp nuget脚手架，待正式版上传

---
+++ Yi.Abp.Net8/tool/Yi.Abp.Tool/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Yi.Abp.Tool;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        try
+        {
+            IHost host = Host.CreateDefaultBuilder()
+                .ConfigureServices(async (host, service) =>
+                {
+                    await service.AddApplicationAsync<YiAbpToolModule>();
+                })
+                .UseAutofac()
+                .Build();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine(ex.StackTrace);
+        }
+
+        if (args.Contains("-v"))
+        {
+            var version = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+            Console.WriteLine($"Yi-ABP CLI {version}");
+        }
+        else
+        {
+            Console.WriteLine("""
+                Usage:
+
+                    yi-abp <command> <target> [options]
+
+                Command List:
+                """);
+        }
+
+    }
+
+}
---
+++ Yi.Abp.Net8/tool/Yi.Abp.Tool/YiAbpToolModule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yi.Abp.Tool
+{
+    public class YiAbpToolModule : AbpModule
+    {
+    }
+}
