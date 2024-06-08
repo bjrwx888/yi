a903b5e36c6a3ba56788089c6e77d502527abb50
橙子 <454313500@qq.com>
2024-06-09 01:10:33 +0800

    fix: 修复https调用http问题

--- Yi.Bbs.Vue3/src/apis/nugetApi.js
+++ Yi.Bbs.Vue3/src/apis/nugetApi.js
@@ -1,7 +1,7 @@
 import request from "@/config/axios/service";
 export function GetResult() {
   return request({
-    url: `http://ccnetcore.com:19002/api/app/nue-get-info/info`,
+    url: `https://ccnetcore.com:19009/api/app/nue-get-info/info`,
     method: "get",
   });
 }
