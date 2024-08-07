bb6e9b9e3f4d1087df1e8ee7d9a03908420a091e
chenchun <chenchun@hytch.com>
2024-08-07 10:16:35 +0800

    feat: 修改存储放入localStorage

--- Yi.RuoYi.Vue3/src/utils/auth.js
+++ Yi.RuoYi.Vue3/src/utils/auth.js
@@ -3,24 +3,24 @@ import Cookies from 'js-cookie'
 const TokenKey = 'Admin-Token'
 const TenantIdKey='Tenant-Id'
 export function getToken() {
-  return Cookies.get(TokenKey)
+  return localStorage.get(TokenKey)
 }
 
 export function setToken(token) {
-  return Cookies.set(TokenKey, token)
+  return localStorage.set(TokenKey, token)
 }
 
 export function removeToken() {
-  return Cookies.remove(TokenKey)
+  return localStorage.remove(TokenKey)
 }
 export function getTenantId() {
-  return Cookies.get(TenantIdKey)
+  return localStorage.get(TenantIdKey)
 }
 
 export function setTenantId(tenantId) {
-  return Cookies.set(TenantIdKey, tenantId)
+  return localStorage.set(TenantIdKey, tenantId)
 }
 
 export function removeTenantId() {
-  return Cookies.remove(TenantIdKey)
+  return localStorage.remove(TenantIdKey)
 }
