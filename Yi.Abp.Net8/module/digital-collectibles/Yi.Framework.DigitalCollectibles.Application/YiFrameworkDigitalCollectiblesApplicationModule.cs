7af54f600f035bc40cce5df8ecf7c9352aa1f960
橙子 <454313500@qq.com>
2024-10-14 21:45:14 +0800

    feat: 搭建模块

--- Yi.Abp.Net8/module/digital-collectibles/Yi.Framework.DigitalCollectibles.Application/YiFrameworkDigitalCollectiblesApplicationModule.cs
+++ Yi.Abp.Net8/module/digital-collectibles/Yi.Framework.DigitalCollectibles.Application/YiFrameworkDigitalCollectiblesApplicationModule.cs
@@ -1,33 +1,28 @@
-﻿using Volo.Abp.SettingManagement;
-using Yi.Abp.Application.Contracts;
-using Yi.Abp.Domain;
+using Yi.Framework.DigitalCollectibles.Application.Contracts;
+using Yi.Framework.DigitalCollectibles.Domain;
 using Yi.Framework.Bbs.Application;
 using Yi.Framework.ChatHub.Application;
 using Yi.Framework.CodeGen.Application;
 using Yi.Framework.Ddd.Application;
-using Yi.Framework.DigitalCollectibles.Application;
 using Yi.Framework.Rbac.Application;
-using Yi.Framework.SettingManagement.Application;
 using Yi.Framework.TenantManagement.Application;
 
-namespace Yi.Abp.Application
+namespace Yi.Framework.DigitalCollectibles.Application
 {
     [DependsOn(
-        typeof(YiAbpApplicationContractsModule),
-        typeof(YiAbpDomainModule),
+        typeof(YiFrameworkDigitalCollectiblesApplicationContractsModule),
+        typeof(YiFrameworkDigitalCollectiblesDomainModule),
 
 
         typeof(YiFrameworkRbacApplicationModule),
          typeof(YiFrameworkBbsApplicationModule),
-        typeof(YiFrameworkDigitalCollectiblesApplicationModule),
          typeof(YiFrameworkChatHubApplicationModule),
         typeof(YiFrameworkTenantManagementApplicationModule),
         typeof(YiFrameworkCodeGenApplicationModule),
-        typeof (YiFrameworkSettingManagementApplicationModule),
 
         typeof(YiFrameworkDddApplicationModule)
         )]
-    public class YiAbpApplicationModule : AbpModule
+    public class YiFrameworkDigitalCollectiblesApplicationModule : AbpModule
     {
     }
 }
--- Yi.Abp.Net8/module/digital-collectibles/Yi.Framework.DigitalCollectibles.SqlSugarCore/YiFrameworkDigitalCollectiblesSqlSugarCoreModule.cs
+++ Yi.Abp.Net8/module/digital-collectibles/Yi.Framework.DigitalCollectibles.SqlSugarCore/YiFrameworkDigitalCollectiblesSqlSugarCoreModule.cs
@@ -1,43 +1,35 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.Modularity;
-using Yi.Abp.Domain;
-using Yi.Abp.SqlSugarCore;
+using Yi.Framework.DigitalCollectibles.Domain;
 using Yi.Framework.AuditLogging.SqlSugarCore;
 using Yi.Framework.Bbs.SqlSugarCore;
 using Yi.Framework.ChatHub.SqlSugarCore;
 using Yi.Framework.CodeGen.SqlSugarCore;
-using Yi.Framework.DigitalCollectibles.SqlsugarCore;
 using Yi.Framework.Mapster;
 using Yi.Framework.Rbac.SqlSugarCore;
-using Yi.Framework.SettingManagement.SqlSugarCore;
 using Yi.Framework.SqlSugarCore;
 using Yi.Framework.SqlSugarCore.Abstractions;
 using Yi.Framework.TenantManagement.SqlSugarCore;
 
-namespace Yi.Abp.SqlsugarCore
+namespace Yi.Framework.DigitalCollectibles.SqlsugarCore
 {
     [DependsOn(
-        typeof(YiAbpDomainModule),
+        typeof(YiFrameworkDigitalCollectiblesDomainModule),
 
         typeof(YiFrameworkRbacSqlSugarCoreModule),
-        typeof(YiFrameworkDigitalCollectiblesSqlSugarCoreModule),
         typeof(YiFrameworkBbsSqlSugarCoreModule),
         typeof(YiFrameworkCodeGenSqlSugarCoreModule),
          typeof(YiFrameworkChatHubSqlSugarCoreModule),
 
-        typeof(YiFrameworkSettingManagementSqlSugarCoreModule),
         typeof(YiFrameworkAuditLoggingSqlSugarCoreModule),
         typeof(YiFrameworkTenantManagementSqlSugarCoreModule),
         typeof(YiFrameworkMapsterModule),
         typeof(YiFrameworkSqlSugarCoreModule)
         )]
-    public class YiAbpSqlSugarCoreModule : AbpModule
+    public class YiFrameworkDigitalCollectiblesSqlSugarCoreModule : AbpModule
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
-            context.Services.AddYiDbContext<YiDbContext>();
-            //默认不开放，可根据项目需要是否Db直接对外开放
-            //context.Services.AddTransient(x => x.GetRequiredService<ISqlSugarDbContext>().SqlSugarClient);
         }
     }
 }
