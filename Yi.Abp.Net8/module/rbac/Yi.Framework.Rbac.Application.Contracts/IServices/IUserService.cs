387fde67cc81c89df816810783ecb91d9508e0c0
simiyu <headone@qq.com>
2024-10-31 10:03:22 +0800

    feat：用户接口上加入装配用户昵称，可以在需要返回创建修改用户名等场景

--- Yi.Abp.Net8/module/rbac/Yi.Framework.Rbac.Application.Contracts/IServices/IUserService.cs
+++ Yi.Abp.Net8/module/rbac/Yi.Framework.Rbac.Application.Contracts/IServices/IUserService.cs
@@ -1,3 +1,4 @@
+using System.Collections.Specialized;
 using Yi.Framework.Ddd.Application.Contracts;
 using Yi.Framework.Rbac.Application.Contracts.Dtos.User;
 
@@ -8,5 +9,20 @@ namespace Yi.Framework.Rbac.Application.Contracts.IServices
     /// </summary>
     public interface IUserService : IYiCrudAppService<UserGetOutputDto, UserGetListOutputDto, Guid, UserGetListInputVo, UserCreateInputVo, UserUpdateInputVo>
     {
+        /// <summary>
+        /// 装配返回数据中的创建用户和修改用户
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dataList">返回数据列表</param>
+        public Task AssemblyBackUser<T>(List<T> dataList) where T : class, IBackUser;
+
+        /// <summary>
+        /// 装配用户，用来处理没有实现IBackUser接口，但需要返回用户信息
+        /// 例如 AuditUserId列 对应 AuditUserNikeName这种
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dataList"></param>
+        /// <param name="keyValues">key为原始id列，value为填充列</param>
+        public Task AssemblyBackUser<T>(List<T> dataList, NameValueCollection keyValues) where T : class;
     }
 }
