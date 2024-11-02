fddf80e74a9578b1b7c23a8e3358b5969f5138cd
橙子 <454313500@qq.com>
2024-11-03 02:38:05 +0800

    feat: 新增商城领域

---
+++ Yi.Abp.Net8/module/bbs/Yi.Framework.Bbs.Application/Services/Shop/BbsShopService.cs
@@ -0,0 +1,12 @@
+﻿using Volo.Abp.Application.Services;
+
+namespace Yi.Framework.Bbs.Application.Services.Shop;
+/// <summary>
+/// bbs商城服务
+/// </summary>
+public class BbsShopService : ApplicationService
+{
+    //获取商城列表
+    //购买道具
+    //获取用户的商城信息
+}
---
+++ Yi.Abp.Net8/module/bbs/Yi.Framework.Bbs.Domain/Managers/Shop/BbsShopManager.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.Domain.Services;
+
+namespace Yi.Framework.Bbs.Domain.Managers.Shop;
+
+/// <summary>
+/// bbs商城领域服务
+/// </summary>
+public class BbsShopManager: DomainService
+{
+    
+}
